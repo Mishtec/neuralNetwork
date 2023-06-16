fe3334bb2420af5656dee0fbe1a6c976b93c2865
Amish Patel <amish920w@gmail.com>
2023-06-16 01:54:23 -0400

    Sigmoid function added

--- PythonApplication1/neuralNetwork.py
+++ PythonApplication1/neuralNetwork.py
@@ -1,9 +1,32 @@
+import numpy as np
+
 # neuralNetwork
 class neuralNetwork:
     def __init__(self):
         self.data = None
 
-    
+    # Sigmoid function 
+    def Sigmoid(x):
+        return 1 / (1 + np.exp(-x))
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+# weights, biases, and the activation function must all be implemented by you
+# a) How many hidden layers have you used?
+# b) What are the weights and biases of each node?    
 
 
 
